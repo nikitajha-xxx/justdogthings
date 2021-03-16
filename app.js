513c0fe36c543802a14fd7c53b150b10dc7f9cf8
Nikita Jha <jhanikita85@gmail.com>
2021-03-16 18:13:28 +0530

    Add route for dogs Page

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.get("/", function(req, res){
 
 app.get("/dogs", function(req, res){
     var dogs = [
-        {name: "HunnyBun", image: ""},
-        {name: "Susie", image: ""},
-        {name: "Tommy", image: ""},
+        {name: "HunnyBun", image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"},
+        {name: "Susie", image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"},
+        {name: "Tommy", image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"}
 
     ]
-    res.send("This works");
+    res.render("dogs");
 });
 
 var listener = app.listen(process.env.PORT, process.env.IP, function(){
