1dd8ef3fb5061758ad4d1ea789ce1885e8c50a93
Nikita Jha <jhanikita85@gmail.com>
2021-03-16 18:56:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/dogs", function(req, res){
         {name: "Tommy", image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"}
 
     ]
-    res.render("dogs");
+    res.render("dogs", {dogs: dogs});
 });
 
 var listener = app.listen(process.env.PORT, process.env.IP, function(){
