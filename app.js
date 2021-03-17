16c126bcbede5bf0c9aee261497095b58bfff820
Nikita Jha <jhanikita85@gmail.com>
2021-03-17 16:14:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var express = require('express');
-
 var app = express();
+
 app.use(express.json({limit: '20mb'}));
 app.use(express.urlencoded({ extended: false, limit: '20mb' }));
-
-
 app.set("view engine", "ejs");
 
 var dogs = [
-    {name: "HunnyBun", image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"},
-    {name: "Susie", image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"},
-    {name: "Tommy", image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"}
+    {name: "HunnyBun", image: "https://images.unsplash.com/photo-1546421845-6471bdcf3edf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nc3xlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"},
+    {name: "Susie", image: "https://images.unsplash.com/photo-1546975490-e8b92a360b24?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGRvZ3N8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"},
+    {name: "Tommy", image: "https://images.unsplash.com/photo-1517443191895-202c31142ccd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fGRvZ3N8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"}
 
 ]
 
@@ -23,11 +21,13 @@ app.get("/dogs", function(req, res){
 });
 
 app.post("/dogs", function(req, res){  //here we send a post request to create a dog post
+    // get data from form and add it to the dogs array
     var name = req.body.name;
     var image = req.body.image;
     var newDog = {name: name, image: image};
     dogs.push(newDog);
-    res.redirect("/dogs");
+    // redirect to the dogs page
+    res.redirect("/dogs"); 
 
 });
 
