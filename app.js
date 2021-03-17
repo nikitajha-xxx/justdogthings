01d4b0859483c42ba10598b059dc87b7defe5f68
Nikita Jha <jhanikita85@gmail.com>
2021-03-17 21:11:50 +0530

    setup Dog model

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var app = express();
+var express     = require('express'),
+    app         = express(),
+    mongoose    = require("mongoose")
 
+
+mongoose.connect("mongodb://localhost:27017/dog_blog", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Connected"))
+.catch(err => console.log(err));
 app.use(express.json({limit: '20mb'}));
 app.use(express.urlencoded({ extended: false, limit: '20mb' }));
 app.set("view engine", "ejs");
+
+//SCHEMA SETUP
+var dogSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Dog = mongoose.model("Dog", dogSchema);
+
+// Dog.create({
+//     name: "Susie", 
+//     image: "https://images.unsplash.com/photo-1546975490-e8b92a360b24?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGRvZ3N8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+// }, function(err, dog){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Newly created dog post");
+//         console.log(dog);
+//     }
+// });
+
+
+
 
 var dogs = [
     {name: "HunnyBun", image: "https://images.unsplash.com/photo-1546421845-6471bdcf3edf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nc3xlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"},
@@ -17,7 +45,15 @@ app.get("/", function(req, res){
 });
 
 app.get("/dogs", function(req, res){
-    res.render("dogs", {dogs: dogs});
+    //get all dog posts from DB
+    Dog.find({}, function(err, alldogs){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("dogs", {dogs: alldogs});
+        }
+    });
 });
 
 app.post("/dogs", function(req, res){  //here we send a post request to create a dog post
@@ -25,10 +61,16 @@ app.post("/dogs", function(req, res){  //here we send a post request to create a
     var name = req.body.name;
     var image = req.body.image;
     var newDog = {name: name, image: image};
-    dogs.push(newDog);
-    // redirect to the dogs page
-    res.redirect("/dogs"); 
-
+    // create a new dog post and save to db
+    Dog.create(newDog, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }
+        else{
+            // redirect to the dogs page
+            res.redirect("/dogs"); 
+        }
+    });
 });
 
 app.get("/dogs/new", function(req, res){
