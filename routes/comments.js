04778e5522170b5eeb2d3a09a6d7120c568d2e09
Nikita Jha <jhanikita85@gmail.com>
2021-03-21 17:47:01 +0530

    Add edit and delete route for comment

--- routes/comments.js
+++ routes/comments.js
@@ -50,12 +50,71 @@ router.post("/", isLoggedIn, function(req, res){
     });
 });
 
+//EDIT ROUTE
+router.get("/:comment_id/edit", checkCommentOwnership , function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back");
+        }else{
+            res.render("comments/edit", {dog_id: req.params.id, comment: foundComment});
+        }
+    });
+    
+});
+
+//Update Comment
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
+       //find and update the correct Comment
+       Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back");
+        } else{
+            res.redirect("/dogs/" + req.params.id);
+        }
+    });
+    //redirect somewhere
+});
+
+//Comment Destroy Route
+router.delete("/:comment_id", checkCommentOwnership,function(req, res){
+    //findByIdAndRemove
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else{
+            res.redirect("/dogs/" + req.params.id);
+        }
+        
+    });
+});
+
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            } else{
+                //does user own the comment
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else{
+                    res.redirect("back");
+                }
+                
+            }
+        });
+    }else{
+        res.redirect("back");
+    }
 }
 
 
